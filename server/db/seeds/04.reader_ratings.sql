ee826377fb6280b4578a772ec5ae2c55e5462256
Ruta Reisoglu <rutareiso@protonmail.com>
2022-02-11 15:42:28 -0500

    Add seeds

---
+++ server/db/seeds/04.reader_ratings.sql
@@ -0,0 +1,6 @@
+insert into reader_ratings (request_id, rating) values (1, 4);
+insert into reader_ratings (request_id, rating) values (4, 4);
+insert into reader_ratings (request_id, rating) values (5, 4);
+insert into reader_ratings (request_id, rating) values (7, 2);
+insert into reader_ratings (request_id, rating) values (11, 3);
+insert into reader_ratings (request_id, rating) values (19, 1);
---
+++ server/db/seeds/05.listener_ratings.sql
@@ -0,0 +1,6 @@
+insert into listener_ratings (request_id, rating) values (1, 3);
+insert into listener_ratings (request_id, rating) values (4, 5);
+insert into listener_ratings (request_id, rating) values (5, 2);
+insert into listener_ratings (request_id, rating) values (7, 5);
+insert into listener_ratings (request_id, rating) values (11, 4);
+insert into listener_ratings (request_id, rating) values (19, 3);
