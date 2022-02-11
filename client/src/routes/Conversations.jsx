8fe15e268e852b3df8eb94a8d41b8f98c1f6f375
Adrian Kiva <akiva@mta.ca>
2022-02-11 18:29:46 -0500

    get server proxy up

--- client/src/routes/Conversations.jsx
+++ client/src/routes/Conversations.jsx
@@ -1,4 +1,13 @@
+import axios from "axios";
+
 const Conversations = (props) => {
+  const conversations = [];
+  axios.get('./messages/')
+  .then(res => 
+    console.log(res.data)
+    
+    )
+  .catch(err => console.log(err))
   return <p>Conversations</p>;
 };
 export default Conversations
