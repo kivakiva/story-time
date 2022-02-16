e9445bec4ddc7b8ba5b62109b64a3fb5c671a8f7
Adrian Kiva <akiva@mta.ca>
2022-02-16 15:04:47 -0500

    style messages

--- client/src/views/Message.jsx
+++ client/src/views/Message.jsx
@@ -1,9 +1,24 @@
 import Timeago from "react-timeago";
 const Message = (props) => {
   const { message, datetimeSent, senderId } = props;
+  const align = () => {
+    if (Math.floor(Math.random() * 2) < 1) {
+      return "self-end bg-secondary rounded-br-none";
+    } else {
+      return "self-start bg-primary rounded-bl-none";
+    }
+  };
   return (
-    <div>
-      {message}, <Timeago date={datetimeSent} />, {senderId}
+    <div className="flex flex-col">
+      <div
+        style={{ color: "#1B3D2F" }}
+        className={`p-3 m-5 rounded-lg ${align()}`}
+      >
+        {message}
+      </div>
+      <div>
+        <Timeago date={datetimeSent} />
+      </div>
     </div>
   );
 };
--- client/src/views/Messages.jsx
+++ client/src/views/Messages.jsx
@@ -8,7 +8,7 @@ const Messages = (props) => {
         id: 1,
         senderId: 1,
         datetimeSent: "2022-01-15",
-        message: "hi nice to meet you",
+        message: "first hello",
       },
       {
         id: 2,
@@ -50,13 +50,22 @@ const Messages = (props) => {
         id: 8,
         senderId: 1,
         datetimeSent: "2022-01-15",
-        message: "hi nice to meet you",
+        message: "goodbye",
       },
     ],
   };
   const parsedMessages = testMessages.messages.map((message) => (
     <Message key={message.id} {...message} />
   ));
-  return <div>{parsedMessages}</div>;
+  return (
+    <div>
+      <div className="fixed left-0 top-0 bg-base-100 z-9 w-full h-24 "></div>
+      <div className="fixed left-0 top-24 z-9 w-full h-8 bg-gradient-to-b from-base-100 via-base-100 to-transparent"></div>
+      <div className="fixed left-0 right-0 ">
+        Chatting with <b>Andy Newman</b>
+      </div>
+      <div className="pt-8">{parsedMessages}</div>
+    </div>
+  );
 };
 export default Messages;
