c03d1faabb0f06a81f4795092b88180ffc888e1d
Adrian Kiva <akiva@mta.ca>
2022-02-17 10:38:41 -0500

    begin implement myreads styling

--- client/src/views/MyReads.jsx
+++ client/src/views/MyReads.jsx
@@ -1,33 +1,60 @@
-import { Link, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Reads from "./Reads";
 import Listens from "./Listens";
 
 const MyReads = (props) => {
-
-  const [state, setState] = useState("listening")
+  const [state, setState] = useState("listening");
 
   const listening = () => {
-    console.log("listening");
-    setState("listening")
-  }
+    setState("listening");
+  };
   const reading = () => {
-    console.log("reading");
-    setState("reading")
-  }
+    setState("reading");
+  };
+  const userID = localStorage.getItem("userID");
+  const [myReads, setMyReads] = useState([]);
+  const [myListens, setMyListens] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`../users/${userID}`)
+      .then((res) => {
+        setMyReads([...res.data.user.all_request_offers]);
+        setMyListens([...res.data.user.all_read_requests]);
+        setError("");
+      })
+      .catch((err) => {
+        setError("Error loading data");
+        setMyReads([]);
+        console.log(err.response.data);
+        console.log(err);
+      });
+  }, [userID]);
+
+  const listenTab = () => {
+    return state === "listening" ? "bg-base-200" : "";
+  };
+
+  const readTab = () => {
+    return state === "reading" ? "bg-base-200" : "";
+  };
+
+  const tab = "rounded-t-xl px-3 pb-2";
 
   return (
     <main>
-      <p>MyReads</p> 
-      <span onClick={()=> listening()}>
-        {state === "listening" ? <b>Listening</b> : <>Listening</> }
-      </span> | 
-      <span onClick={() => reading()}>
-        { state === "reading" ? <b>Reading</b> : <>Reading</> }
+      <p>MyReads</p>
+      <span onClick={() => listening()} className={`${tab} ${listenTab()}`}>
+        <>Listening</>
+      </span>{" "}
+      <span onClick={() => reading()} className={`${tab} ${readTab()}`}>
+        <>Reading</>
       </span>
-      { state === "reading" && <Reads /> }
-      { state === "listening" && <Listens /> }
+      {state === "reading" && <Reads myReads={myReads} />}
+      {state === "listening" && <Listens myListens={myListens} />}
     </main>
-  )
-}
-export default MyReads
+  );
+};
+export default MyReads;
