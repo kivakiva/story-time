b8b79aef3d6abd6fca1043657dc98f2604447a01
Ruta Reisoglu <rutareiso@protonmail.com>
2022-02-17 20:18:35 -0500

    Navigate to previous page

--- client/src/views/shared/UpdateOfferButtons.jsx
+++ client/src/views/shared/UpdateOfferButtons.jsx
@@ -10,7 +10,7 @@ const UpdateOfferButtons = ({ offerID }) => {
   const cancelOffer = () => {
     try {
       axios.delete(`/reads/${offerID}`);
-      navigate("/");
+      navigate(-1);
     } catch (err) {
       console.log(err);
     }
