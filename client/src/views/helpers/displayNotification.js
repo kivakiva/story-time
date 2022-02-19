2498ee99ba81663c506a6c366eb0a9d07a2570e7
DonThePhan <donthephan>
2022-02-19 16:48:23 -0500

    create displayNotification to simplify Notification function

---
+++ client/src/views/helpers/displayNotification.js
@@ -0,0 +1,16 @@
+import { Store } from "react-notifications-component";
+import Notification from "../shared/Notification";
+
+const displayNotification = (message) => {
+  Store.addNotification({
+    content: <Notification message={`TEST! ${message}`} />,
+    container: "center",
+    animationIn: ["animate__animated animate__fadeIn"],
+    animationOut: ["animate__animated animate__fadeOut"],
+    dismiss: {
+      duration: 2000,
+    },
+  });
+};
+
+export default displayNotification;
