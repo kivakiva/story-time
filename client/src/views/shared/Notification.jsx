c8d98f7f74ed0343431b7f9745ffe7368ef6f814
DonThePhan <donthephan>
2022-02-19 16:50:40 -0500

    remove test message from Notification component

--- client/src/views/shared/Notification.jsx
+++ client/src/views/shared/Notification.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Notification = ({ message }) => {
   return (
     <div className="py-3 bg-base-100 w-full border border-solid border-black">
-      <p>{message} hiasdfad</p>
+      <p>{message}</p>
     </div>
   );
 };
