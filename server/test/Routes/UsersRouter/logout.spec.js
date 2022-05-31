ab40fe8d41a972212a7c7db1297ada814ebf960e
Ruta Reisoglu <rutareiso@protonmail.com>
2022-05-31 14:54:13 -0400

    Add tests for UsersRouter logout function

---
+++ server/test/Routes/UsersRouter/logout.spec.js
@@ -0,0 +1,20 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../../../app");
+
+chai.should();
+chai.use(chaiHttp);
+
+describe("UsersRouter logout function", () => {
+  it("It should log out user", (done) => {
+    chai
+      .request(server)
+      .post("/api/users/logout")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("message").eql("User logged out!");
+        done();
+      });
+  });
+});
