86f9cc118affe90a8b743aea3f7018bee11a44bc
Ruta Reisoglu <rutareiso@protonmail.com>
2022-06-01 07:35:37 -0400

    Refactor database reset

--- server/db/resetdb.js
+++ server/db/resetdb.js
@@ -7,9 +7,15 @@ const chalk = require("chalk");
 const Client = require("pg-native");
 
 // PG connection setup
-const connectionString =
-  process.env.DATABASE_URL ||
-  `postgresql://${process.env.DB_USER}:${process.env.DB_TEST_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+let connectionString;
+if (process.env.NODE_ENV === "test") {
+  console.log("Test environment");
+  connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_TEST}?sslmode=disable`;
+} else {
+  connectionString =
+    process.env.DATABASE_URL ||
+    `postgresql://${process.env.DB_USER}:${process.env.DB_TEST_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+}
 const client = new Client();
 
 // Loads the schema files from db/schema
