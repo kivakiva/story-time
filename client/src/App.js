80f2bde8f1571292ed670ad75bb766c7a5448a2e
donthephan <donthephan@gmail.com>
2022-10-27 13:15:27 -0400

    change chat font from dark grey to white

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "./views/shared/Header";
 import Footer from "./views/shared/Footer";
 import { ReactNotifications } from "react-notifications-component";
-import Notification from "./views/shared/Notification";
 import "./App.css";
 
 function App() {
